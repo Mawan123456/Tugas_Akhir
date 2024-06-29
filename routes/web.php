cfbe8618310718897c77f94207ef6a8d06f934a1
akrommm <102010542+akrommm@users.noreply.github.com>
2024-06-29 15:46:52 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::redirect('/', 'login');
+
 // Login
 Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::post('login', [AuthController::class, 'loginProcess']);
